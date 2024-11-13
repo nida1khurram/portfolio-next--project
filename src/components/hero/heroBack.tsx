1b28ecc6f6acac3527fe340ea2716f78f7edc544
nida1khurram <147607279+nida1khurram@users.noreply.github.com>
2024-11-13 20:21:35 +0500

    pic update

--- src/components/hero/heroBack.tsx
+++ src/components/hero/heroBack.tsx
@@ -22,7 +22,7 @@ export function VortexDemoSecond() {
               width={200}
               height={200}
               alt="logo"
-              src="/picture/hero.png"
+              src="/picture/hero/hero.png"
               className=""
             />
           </div>
